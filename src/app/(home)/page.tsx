ff527b4717fb6fcad2ea3c44781babad56a87727
AlexRobertPetrovic <alexrobertpetrovic@gmail.com>
2024-09-19 08:47:17 +0200

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+
+// robertweb/src/app/(home)/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: 'Domov | RobertWeb'};
+
+export default function Home() {
+  return (
+
+      <Typography> Domovska stranka </Typography>
+
+  );
+}
